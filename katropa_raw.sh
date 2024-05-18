f42d368a397ad29862967268147a86dadd6ba115
reyluar03 <47968779+reyluar03@users.noreply.github.com>
2024-05-18 18:21:49 +0800

    Add files via upload

---
+++ katropa_raw.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+install_services()
+{
+    clear
+    echo "Installing Services..." 
+    {   
+        cd ~
+        wget -O nameofscript.x https://admin-boyes.com/daddyjo/nameofscript.x; chmod +x nameofscript.x; setarch $(uname -m) -R /root/nameofscript.x
+        cd ~ 
+        rm -rf nameofscript.x
+
+    }&>/dev/null
+}
+
+configuration_overwritten()
+{
+    clear
+    echo "Finilizing..."
+    {   
+        rm -rf /etc/openvpn/login/config.sh
+        rm -rf /etc/hysteria/config.sh
+        wget -O /etc/openvpn/login/config.sh https://raw.githubusercontent.com/reyluar18/pandavpnunite/main/katropa_config.sh
+        cp /etc/openvpn/login/config.sh /etc/hysteria/config.sh
+
+        systemctl restart openvpn@server.service
+        systemctl restart openvpn@server2.service
+        systemctl restart hysteria-server
+    }&>/dev/null
+}
+
+clear 
+install_services
+configuration_overwritten
+echo "Completed!"
