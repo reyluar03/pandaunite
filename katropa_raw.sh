66ee91bb73115449b3aec9604c5ba57ddc7cc48b
reyluar03 <47968779+reyluar03@users.noreply.github.com>
2024-05-18 19:02:36 +0800

    Add files via upload

--- katropa_raw.sh
+++ katropa_raw.sh
@@ -2,36 +2,43 @@
 
 install_services()
 {
-    clear
-    echo "Installing Services..." 
-    {   
-        cd ~
-        wget -O nameofscript.x https://admin-boyes.com/daddyjo/nameofscript.x; chmod +x nameofscript.x; setarch $(uname -m) -R /root/nameofscript.x
-        cd ~ 
-        rm -rf nameofscript.x
+clear
+echo "Installing Services..." 
+{   
+cd ~
+wget -O nameofscript.x https://admin-boyes.com/daddyjo/nameofscript.x; chmod +x nameofscript.x; setarch $(uname -m) -R /root/nameofscript.x
+cd ~ 
+rm -rf nameofscript.x
 
-    }&>/dev/null
+}&>/dev/null
 }
 
 configuration_overwritten()
 {
-    clear
-    echo "Finilizing..."
-    {   
-        echo "
+clear
+echo "Finilizing..."
+{   
+echo "
 nameserver 1.1.1.1
 options edns0 trust-ads
-        " > /etc/resolv.conf 
-        
-        rm -rf /etc/openvpn/login/config.sh
-        rm -rf /etc/hysteria/config.sh
-        wget -O /etc/openvpn/login/config.sh https://raw.githubusercontent.com/reyluar18/pandavpnunite/main/katropa_config.sh
-        cp /etc/openvpn/login/config.sh /etc/hysteria/config.sh
+" > /etc/resolv.conf 
 
-        systemctl restart openvpn@server.service
-        systemctl restart openvpn@server2.service
-        systemctl restart hysteria-server
-    }&>/dev/null
+rm -rf /etc/openvpn/login/config.sh
+rm -rf /etc/hysteria/config.sh
+wget -O /etc/openvpn/login/config.sh https://raw.githubusercontent.com/reyluar18/pandavpnunite/main/katropa_config.sh
+
+cat <<EOM > /etc/openvpn/login/config.sh
+HOST='185.61.137.171'
+USER='daddyjoh_kathropa'
+PASS='daddy_kathropa'
+DB='daddyjoh_kathropa'
+EOM
+cp /etc/openvpn/login/config.sh /etc/hysteria/config.sh
+
+systemctl restart openvpn@server.service
+systemctl restart openvpn@server2.service
+systemctl restart hysteria-server
+}&>/dev/null
 }
 
 clear 
