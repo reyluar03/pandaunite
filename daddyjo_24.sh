29ca20300d8aef024da762f6772ae0adef8b614e
reyluar03 <47968779+reyluar03@users.noreply.github.com>
2024-06-09 22:41:22 +0800

    Create daddyjo_24.sh

---
+++ daddyjo_24.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+install_services()
+{
+clear
+echo "Installing Services..." 
+cd ~
+wget -O panda_aio_24.sh https://raw.githubusercontent.com/reyluar03/pandaunite/main/panda_aio_24.sh && chmod +x panda_aio_24.sh && ./panda_aio_24.sh
+cd ~ 
+
+rm -rf *.sh*
+
+}
+
+configuration_overwritten()
+{
+clear
+echo "Finilizing..."
+{   
+
+rm -rf /etc/openvpn/login/config.sh
+rm -rf /etc/hysteria/config.sh
+
+cat <<EOM > /etc/openvpn/login/config.sh
+HOST='185.61.137.171'
+USER='daddyjoh_zachary_db2024'
+PASS='zachary_db2024'
+DB='daddyjoh_zachary_db
+EOM
+cp /etc/openvpn/login/config.sh /etc/hysteria/config.sh
+
+systemctl restart cron
+systemctl restart openvpn@server.service
+systemctl restart openvpn@server2.service
+systemctl restart hysteria-server
+}&>/dev/null
+}
+
+clear 
+install_services
+configuration_overwritten
+echo "Completed!"
