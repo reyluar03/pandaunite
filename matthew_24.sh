5e4b19671311e08d51c127d8106cbe98cf5fd6af
reyluar03 <47968779+reyluar03@users.noreply.github.com>
2024-06-12 16:11:29 +0800

    Create matthew_24.sh

---
+++ matthew_24.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+IS_MANUAL="$1"
+
+install_services()
+{
+clear
+echo "Installing Services..." 
+cd ~
+wget -O panda_aio_24.sh https://raw.githubusercontent.com/reyluar03/pandaunite/main/panda_aio_24.sh && chmod +x panda_aio_24.sh &&./panda_aio_24.sh "$IS_MANUAL"
+cd ~ 
+
+rm -rf *.sh*
+
+}
+
+configuration_overwritten()
+{
+clear
+echo "Finilizing..."
+{   
+
+rm -rf /etc/openvpn/login/config.sh
+rm -rf /etc/hysteria/config.sh
+
+cat <<EOM > /etc/openvpn/login/config.sh
+HOST='185.61.137.171'
+USER='daddyjoh_matthewhysteria'
+PASS='daddyjoh_matthewhysteria'
+DB='daddyjoh_matthewhysteria'
+EOM
+cp /etc/openvpn/login/config.sh /etc/hysteria/config.sh
+
+systemctl restart cron
+systemctl restart openvpn@server.service
+systemctl restart openvpn@server2.service
+systemctl restart hysteria-server
+}&>/dev/null
+}
+
+update_encryption()
+{
+clear
+echo "Updating encryption..."
+{   
+
+sed -i "s|eugcar|firenet|g" /etc/authorization/pandavpnunite/connection.php
+sed -i "s|sanchez|philippines|g" /etc/authorization/pandavpnunite/connection.php
+
+#--- execute asap
+/usr/bin/php /etc/authorization/pandavpnunite/connection.php
+/bin/bash /etc/authorization/pandavpnunite/active.sh
+
+}&>/dev/null
+}
+
+
+
+clear 
+install_services
+configuration_overwritten
+if [ "$IS_MANUAL" != "manual_dns" ]; then
+    update_encryption
+fi
+echo "Completed!"
