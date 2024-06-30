30354682993572cce996eb6b0c5944c793986a2f
reyluar03 <47968779+reyluar03@users.noreply.github.com>
2024-06-30 10:58:48 +0800

    Update debian_all_team.sh

--- debian_all_team.sh
+++ debian_all_team.sh
@@ -62,6 +62,15 @@ sed -i "s|sanchez|philippines|g" /etc/authorization/pandavpnunite/connection.php
 cp /etc/authorization/pandavpnunite/connection.php /etc/authorization/pandavpnunite/connection3.php
 sed -i "s|login/config.sh|login/test_config2.sh|g" /etc/authorization/pandavpnunite/connection3.php
 
+sed -i "s|/etc/authorization/pandavpnunite/active.sh|/etc/authorization/pandavpnunite/active2.sh|g" /etc/authorization/pandavpnunite/connection.php
+sed -i "s|/etc/authorization/pandavpnunite/uuid.sh|/etc/authorization/pandavpnunite/uuid2.sh|g" /etc/authorization/pandavpnunite/connection.php
+sed -i "s|/etc/authorization/pandavpnunite/not-active.sh|/etc/authorization/pandavpnunite/not-active2.sh|g" /etc/authorization/pandavpnunite/connection.php
+
+sed -i "s|/etc/authorization/pandavpnunite/active.sh|/etc/authorization/pandavpnunite/active3.sh|g" /etc/authorization/pandavpnunite/connection3.php
+sed -i "s|/etc/authorization/pandavpnunite/uuid.sh|/etc/authorization/pandavpnunite/uuid3.sh|g" /etc/authorization/pandavpnunite/connection3.php
+sed -i "s|/etc/authorization/pandavpnunite/not-active.sh|/etc/authorization/pandavpnunite/not-active3.sh|g" /etc/authorization/pandavpnunite/connection3.php
+
+
 #--- execute asap
 /usr/bin/php /etc/authorization/pandavpnunite/connection.php
 /bin/bash /etc/authorization/pandavpnunite/active.sh
@@ -78,16 +87,15 @@ SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 * * * * * pgrep -x stunnel4 >/dev/null && echo 'GOOD' || /etc/init.d/stunnel4 restart
 * * * * * /usr/bin/php /etc/authorization/pandavpnunite/connection.php >/etc/authorization/pandavpnunite/log/connection.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/active.sh >/etc/authorization/pandavpnunite/log/active.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active.sh >/etc/authorization/pandavpnunite/log/inactive.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/active2.sh >/etc/authorization/pandavpnunite/log/active2.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active2.sh >/etc/authorization/pandavpnunite/log/inactive2.log 2>&1
 * * * * * /bin/bash /etc/authorization/pandavpnunite/v2ray.sh >/etc/authorization/pandavpnunite/log/v2ray.log 2>&1
 * * * * * /usr/bin/php /etc/authorization/pandavpnunite/connection2.php >/etc/authorization/pandavpnunite/log/connection2.log 2>&1
 * * * * * /bin/bash /etc/authorization/pandavpnunite/active.sh >/etc/authorization/pandavpnunite/log/active.log 2>&1
 * * * * * /bin/bash /etc/authorization/pandavpnunite/not-active.sh >/etc/authorization/pandavpnunite/log/inactive.log 2>&1
 * * * * * /usr/bin/php /etc/authorization/pandavpnunite/connection3.php >/etc/authorization/pandavpnunite/log/connection3.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/active.sh >/etc/authorization/pandavpnunite/log/active.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active.sh >/etc/authorization/pandavpnunite/log/inactive.log 2>&1
-* * * * * /usr/bin/php /etc/authorization/pandavpnunite/v2ray.php >/etc/authorization/pandavpnunite/log/v2ray_auth.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/active3.sh >/etc/authorization/pandavpnunite/log/active3.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active3.sh >/etc/authorization/pandavpnunite/log/inactive3.log 2>&1
 * * * * * /usr/bin/python /etc/authorization/pandavpnunite/v2ray_up.py --file_name v2ray.txt >/etc/authorization/pandavpnunite/log/v2ray_up.log 2>&1
 
 "; 
