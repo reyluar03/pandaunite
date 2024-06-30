8838a20543c0e222ce5e3ade446ede52cbd6f1c7
reyluar03 <47968779+reyluar03@users.noreply.github.com>
2024-06-30 09:27:26 +0800

    Create ubuntu_all_team.sh

---
+++ ubuntu_all_team.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+IS_MANUAL="$1"
+
+install_services()
+{
+clear
+echo "Installing Services..." 
+cd ~
+wget -O panda_aio_24.sh https://raw.githubusercontent.com/reyluar03/pandaunite/main/panda_aio_24.sh && chmod +x panda_aio_24.sh &&./panda_aio_24.sh "$IS_MANUAL"
+cd ~ 
+
+rm -rf *.sh*
+
+}
+
+configuration_overwritten()
+{
+clear
+echo "Finilizing..."
+{   
+
+rm -rf /etc/openvpn/login/config.sh
+rm -rf /etc/openvpn/login/auth_vpn
+cp 
+wget --no-check-certificate --no-cache --no-cookies -O /etc/openvpn/login/auth_vpn "https://raw.githubusercontent.com/reyluar03/pandaunite/main/auth_vpn"
+
+
+cat <<EOM > /etc/openvpn/login/config.sh
+HOST='185.61.137.171'
+USER='daddyjoh_store2024'
+PASS='daddyjoh_store2024'
+DB='daddyjoh_store2024'
+EOM
+
+cat <<EOM > /etc/openvpn/login/test_config2.sh
+HOST='185.61.137.171'
+USER='daddyjoh_zachary_db2024'
+PASS='zachary_db2024'
+DB='daddyjoh_zachary_db'
+EOM
+
+rm -rf /etc/hysteria/.auth.sh
+cp /etc/openvpn/login/auth_vpn /etc/hysteria/.auth.sh
+
+systemctl restart cron
+systemctl restart openvpn@server.service
+systemctl restart openvpn@server2.service
+systemctl restart hysteria-server
+}&>/dev/null
+}
+
+update_encryption()
+{
+clear
+echo "Updating encryption..."
+{   
+
+sed -i "s|eugcar|firenet|g" /etc/authorization/pandavpnunite/connection.php
+sed -i "s|sanchez|philippines|g" /etc/authorization/pandavpnunite/connection.php
+
+cp /etc/authorization/pandavpnunite/connection.php /etc/authorization/pandavpnunite/connection3.php
+sed -i "s|login/config.sh|login/test_config2.sh|g" /etc/authorization/pandavpnunite/connection3.php
+
+#--- execute asap
+/usr/bin/php /etc/authorization/pandavpnunite/connection.php
+/bin/bash /etc/authorization/pandavpnunite/active.sh
+
+#--- execute asap
+/usr/bin/php /etc/authorization/pandavpnunite/connection3.php
+/bin/bash /etc/authorization/pandavpnunite/active.sh
+
+}&>/dev/null
+}
+
+sudo crontab -l | { echo "
+SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+* * * * * pgrep -x stunnel4 >/dev/null && echo 'GOOD' || /etc/init.d/stunnel4 restart
+* * * * * /usr/bin/php /etc/authorization/pandavpnunite/connection.php >/etc/authorization/pandavpnunite/log/connection.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/active.sh >/etc/authorization/pandavpnunite/log/active.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active.sh >/etc/authorization/pandavpnunite/log/inactive.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/v2ray.sh >/etc/authorization/pandavpnunite/log/v2ray.log 2>&1
+* * * * * /usr/bin/php /etc/authorization/pandavpnunite/connection2.php >/etc/authorization/pandavpnunite/log/connection2.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/active.sh >/etc/authorization/pandavpnunite/log/active.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active.sh >/etc/authorization/pandavpnunite/log/inactive.log 2>&1
+* * * * * /usr/bin/php /etc/authorization/pandavpnunite/connection3.php >/etc/authorization/pandavpnunite/log/connection3.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/active.sh >/etc/authorization/pandavpnunite/log/active.log 2>&1
+* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active.sh >/etc/authorization/pandavpnunite/log/inactive.log 2>&1
+* * * * * /usr/bin/php /etc/authorization/pandavpnunite/v2ray.php >/etc/authorization/pandavpnunite/log/v2ray_auth.log 2>&1
+* * * * * /usr/bin/python /etc/authorization/pandavpnunite/v2ray_up.py --file_name v2ray.txt >/etc/authorization/pandavpnunite/log/v2ray_up.log 2>&1
+
+"; 
+} | crontab -
+
+
+clear 
+install_services
+configuration_overwritten
+update_encryption
+
+echo "Completed!"
