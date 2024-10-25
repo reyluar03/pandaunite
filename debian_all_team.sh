68e7069ff8dae43dc3a9278c4463c9ba3b11d826
reyluar03 <47968779+reyluar03@users.noreply.github.com>
2024-10-25 17:11:50 +0430

    Update debian_all_team.sh

--- debian_all_team.sh
+++ debian_all_team.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-IS_MANUAL="$1"
-
-install_services()
-{
-clear
-echo "Installing Services..." 
-cd ~
-wget --no-check-certificate --no-cache --no-cookies -O /root/dnst_count.txt "https://raw.githubusercontent.com/reyluar03/pandaunite/main/dns_count.txt"
-
-wget -O panda_deb_aio_12.sh https://raw.githubusercontent.com/reyluar03/pandaunite/main/panda_deb_aio_12.sh && chmod +x panda_deb_aio_12.sh &&./panda_deb_aio_12.sh "$IS_MANUAL"
-cd ~ 
-
-rm -rf *.sh*
-
-}
-
 configuration_overwritten()
 {
 clear
@@ -52,6 +36,10 @@ systemctl restart cron
 systemctl restart openvpn@server.service
 systemctl restart openvpn@server2.service
 systemctl restart hysteria-server
+
+wget --no-check-certificate --no-cache --no-cookies -O /etc/authorization/scriptsrelease/account_cleanup.sh "https://raw.githubusercontent.com/scripts-release/vpn-server/main/account_cleanup.sh"
+chmod +x /etc/authorization/scriptsrelease/account_cleanup.sh
+
 }&>/dev/null
 }
 
@@ -61,45 +49,45 @@ clear
 echo "Updating encryption..."
 {   
 
-sed -i "s|eugcar|firenet|g" /etc/authorization/pandavpnunite/connection.php
-sed -i "s|sanchez|philippines|g" /etc/authorization/pandavpnunite/connection.php
+sed -i "s|eugcar|firenet|g" /etc/authorization/scriptsrelease/connection.php
+sed -i "s|sanchez|philippines|g" /etc/authorization/scriptsrelease/connection.php
 
-cp /etc/authorization/pandavpnunite/connection.php /etc/authorization/pandavpnunite/connection3.php
-sed -i "s|login/config.sh|login/test_config2.sh|g" /etc/authorization/pandavpnunite/connection3.php
+cp /etc/authorization/scriptsrelease/connection.php /etc/authorization/scriptsrelease/connection3.php
+sed -i "s|login/config.sh|login/test_config2.sh|g" /etc/authorization/scriptsrelease/connection3.php
 
-sed -i "s|/etc/authorization/pandavpnunite/active.sh|/etc/authorization/pandavpnunite/active2.sh|g" /etc/authorization/pandavpnunite/connection.php
-sed -i "s|/etc/authorization/pandavpnunite/uuid.sh|/etc/authorization/pandavpnunite/uuid2.sh|g" /etc/authorization/pandavpnunite/connection.php
-sed -i "s|/etc/authorization/pandavpnunite/not-active.sh|/etc/authorization/pandavpnunite/not-active2.sh|g" /etc/authorization/pandavpnunite/connection.php
+sed -i "s|/etc/authorization/scriptsrelease/active.sh|/etc/authorization/scriptsrelease/active2.sh|g" /etc/authorization/scriptsrelease/connection.php
+sed -i "s|/etc/authorization/scriptsrelease/uuid.sh|/etc/authorization/scriptsrelease/uuid2.sh|g" /etc/authorization/scriptsrelease/connection.php
+sed -i "s|/etc/authorization/scriptsrelease/not-active.sh|/etc/authorization/scriptsrelease/not-active2.sh|g" /etc/authorization/scriptsrelease/connection.php
 
-sed -i "s|/etc/authorization/pandavpnunite/active.sh|/etc/authorization/pandavpnunite/active3.sh|g" /etc/authorization/pandavpnunite/connection3.php
-sed -i "s|/etc/authorization/pandavpnunite/uuid.sh|/etc/authorization/pandavpnunite/uuid3.sh|g" /etc/authorization/pandavpnunite/connection3.php
-sed -i "s|/etc/authorization/pandavpnunite/not-active.sh|/etc/authorization/pandavpnunite/not-active3.sh|g" /etc/authorization/pandavpnunite/connection3.php
+sed -i "s|/etc/authorization/scriptsrelease/active.sh|/etc/authorization/scriptsrelease/active3.sh|g" /etc/authorization/scriptsrelease/connection3.php
+sed -i "s|/etc/authorization/scriptsrelease/uuid.sh|/etc/authorization/scriptsrelease/uuid3.sh|g" /etc/authorization/scriptsrelease/connection3.php
+sed -i "s|/etc/authorization/scriptsrelease/not-active.sh|/etc/authorization/scriptsrelease/not-active3.sh|g" /etc/authorization/scriptsrelease/connection3.php
 
-
-#--- execute asap
-/usr/bin/php /etc/authorization/pandavpnunite/connection.php
-/bin/bash /etc/authorization/pandavpnunite/active2.sh
 
 #--- execute asap
-/usr/bin/php /etc/authorization/pandavpnunite/connection3.php
-/bin/bash /etc/authorization/pandavpnunite/active3.sh
+/usr/bin/php /etc/authorization/scriptsrelease/connection.php
+/bin/bash /etc/authorization/scriptsrelease/active2.sh
+
+#--- execute asap
+/usr/bin/php /etc/authorization/scriptsrelease/connection3.php
+/bin/bash /etc/authorization/scriptsrelease/active3.sh
 
 sudo crontab -l | { echo "
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 * * * * * pgrep -x stunnel4 >/dev/null && echo 'GOOD' || /etc/init.d/stunnel4 restart
-* * * * * /usr/bin/php /etc/authorization/pandavpnunite/connection.php >/etc/authorization/pandavpnunite/log/connection.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/active2.sh >/etc/authorization/pandavpnunite/log/active2.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active2.sh >/etc/authorization/pandavpnunite/log/inactive2.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/v2ray.sh >/etc/authorization/pandavpnunite/log/v2ray.log 2>&1
-* * * * * /usr/bin/php /etc/authorization/pandavpnunite/connection2.php >/etc/authorization/pandavpnunite/log/connection2.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/active.sh >/etc/authorization/pandavpnunite/log/active.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active.sh >/etc/authorization/pandavpnunite/log/inactive.log 2>&1
-* * * * * /usr/bin/php /etc/authorization/pandavpnunite/connection3.php >/etc/authorization/pandavpnunite/log/connection3.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/active3.sh >/etc/authorization/pandavpnunite/log/active3.log 2>&1
-* * * * * /bin/bash /etc/authorization/pandavpnunite/not-active3.sh >/etc/authorization/pandavpnunite/log/inactive3.log 2>&1
-* * * * * /usr/bin/python /etc/authorization/pandavpnunite/v2ray_up.py --file_name v2ray.txt >/etc/authorization/pandavpnunite/log/v2ray_up.log 2>&1
-
+* * * * * /usr/bin/php /etc/authorization/scriptsrelease/connection.php >/etc/authorization/scriptsrelease/log/connection.log 2>&1
+* * * * * /bin/bash /etc/authorization/scriptsrelease/active2.sh >/etc/authorization/scriptsrelease/log/active2.log 2>&1
+* * * * * /bin/bash /etc/authorization/scriptsrelease/not-active2.sh >/etc/authorization/scriptsrelease/log/inactive2.log 2>&1
+* * * * * /bin/bash /etc/authorization/scriptsrelease/v2ray.sh >/etc/authorization/scriptsrelease/log/v2ray.log 2>&1
+* * * * * /usr/bin/php /etc/authorization/scriptsrelease/connection2.php >/etc/authorization/scriptsrelease/log/connection2.log 2>&1
+* * * * * /bin/bash /etc/authorization/scriptsrelease/active.sh >/etc/authorization/scriptsrelease/log/active.log 2>&1
+* * * * * /bin/bash /etc/authorization/scriptsrelease/not-active.sh >/etc/authorization/scriptsrelease/log/inactive.log 2>&1
+* * * * * /usr/bin/php /etc/authorization/scriptsrelease/connection3.php >/etc/authorization/scriptsrelease/log/connection3.log 2>&1
+* * * * * /bin/bash /etc/authorization/scriptsrelease/active3.sh >/etc/authorization/scriptsrelease/log/active3.log 2>&1
+* * * * * /bin/bash /etc/authorization/scriptsrelease/not-active3.sh >/etc/authorization/scriptsrelease/log/inactive3.log 2>&1
+* * * * * /usr/bin/python /etc/authorization/scriptsrelease/v2ray_up.py --file_name v2ray.txt >/etc/authorization/scriptsrelease/log/v2ray_up.log 2>&1
+*/5 * * * * /bin/bash /etc/authorization/scriptsrelease/account_cleanup.sh
 @reboot /bin/bash /usr/local/sbin/startup.sh
 
 "; 
@@ -112,7 +100,6 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/us
 
 
 clear 
-install_services
 configuration_overwritten
 update_encryption
 
